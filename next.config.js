7b0ccd99abff39578390b3485953e68154ab9ccc
Vicsicard <vicsicard@gmail.com>
2024-12-06 21:11:30 -0700

    Simplify next.config.js to focus on app directory support

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  distDir: '.next',
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        '@': path.join(__dirname, 'src'),
-      },
-    };
-    return config;
+  experimental: {
+    appDir: true
   },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
